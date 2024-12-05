60b2db239b0919bb85f5952f6dd1207d08565578
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-12-05 15:38:14 +0300

    changedApiToRender

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // const devUrl = 'http://localhost:5500/api';
-const prodUrl = 'https://residential-27ef77617daa.herokuapp.com/api';
-// const prodUrl = 'https://house-management-backend.onrender.com/api';
+// const prodUrl = 'https://residential-27ef77617daa.herokuapp.com/api';
+const prodUrl = 'https://vrhmsresidentialbackend.onrender.com/api';
 
 const apiRequest = axios.create({
   baseURL: prodUrl,
